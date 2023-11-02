1fb87972ee3524dd4a24f6d555bc77bfce89b34b
Patrick Annan <you@example.compatrickannan@hotmail.com>
2023-11-01 20:05:15 -0400

    upload

---
+++ text.js
@@ -0,0 +1,34 @@
+alert(Hello, World!);
+
+/*
+alert(Hello, World!);
+*/
+
+// alert(Hello, World!);
+
+// make the image invisible
+// move the image from top to bottom
+// make the image visible
+
+var myVariable = 10
+var myVar2 = "hello"
+myVar2 = "hello again"
+myVar2 = 5;
+var customerAge = 32;
+var myVar3 = 40;
+
+var myVar = 5
+myVar = myVar +10
+
+
+var myVar = 10
+myVar = myVar + 5
+myVar +=5
+myVar += 1
+MyVar ++
+
+var myVar = "Patrick"
+document.write(myVar)
+console.log(50)
+document.write(20/5)
+
